012bbfb73a1d878d215ea0455a386ab0a41c3069
niqdev <niqdev@users.noreply.github.com>
2023-01-04 00:17:13 +0000

    update mylog

--- internal/mylog/mylog.go
+++ internal/mylog/mylog.go
@@ -9,9 +9,11 @@ import (
 
 // https://pkg.go.dev/go.uber.org/zap
 // https://github.com/sandipb/zap-examples
+// https://github.com/pete911/zap-examples
+// https://github.com/snowzach/gorestapi
 // https://gist.github.com/calam1/c2673b6b0a53918df033d71bbf958b56
-// https://github.com/bigwhite/experiments/tree/master/uber-zap-advanced-usage
-// COLOR https://github.com/uber-go/zap/issues/648
+// ROTATION https://github.com/bigwhite/experiments/blob/master/uber-zap-advanced-usage/demo4/pkg/log/log.go
+// https://gist.github.com/reitzig/2d53098861b6c1b7fcdfb065b4a81f2d
 
 func ExampleFromDoc() {
 	// Using zap's preset constructors is the simplest way to get a feel for the
@@ -43,6 +45,7 @@ func ExampleFromDoc() {
 	)
 }
 
+// COLOR https://github.com/uber-go/zap/issues/648
 func ExampleWithColor() {
 	config := zap.NewDevelopmentConfig()
 	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
@@ -51,3 +54,7 @@ func ExampleWithColor() {
 	sugar := logger.Sugar()
 	sugar.Info("example")
 }
+
+// VERBOSE
+// https://pkg.go.dev/k8s.io/klog
+// https://gist.github.com/toddlers/2fa8fcb1d36e9e132b4275f2d748f39f
