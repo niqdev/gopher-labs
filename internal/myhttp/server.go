98d41d738c0ddbf9594865efe6f96e93d3743bb1
niqdev <niqdev@users.noreply.github.com>
2024-01-12 17:33:35 +0000

    draft static server

--- internal/myhttp/server.go
+++ internal/myhttp/server.go
@@ -5,24 +5,24 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 // https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
 
 func StartServer() {
-	log.Println("TODO not implemented")
+	log.Println("listening on port 3333")
 
-	http.HandleFunc("/", getRoot)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/hello", getHello)
 
-	if err := http.ListenAndServe(":3333", nil); err != nil {
+	server := &http.Server{
+		Addr:              ":3333",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("error %s", err)
 	}
-}
-
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("new root request\n")
-	io.WriteString(w, "ROOT\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
